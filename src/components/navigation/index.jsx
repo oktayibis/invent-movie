b8ffd41fd540f7eece5e59ca02189760cd22a46b
Oktay Ibis <oktayibis@gmail.com>
2020-09-19 03:13:04 +0300

    table ve card view options added

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,8 +1,19 @@
+import { Link, Container } from "@material-ui/core";
 import React from "react";
 
 import Search from "./searchMovie";
 function Index() {
-  return <Search />;
+  return (
+    <div>
+      <Container style={{ marginBottom: 10 }}>
+        <Link href="/table">For Table View</Link>
+        <Link style={{ marginLeft: 10 }} href="/">
+          For Card View
+        </Link>
+      </Container>
+      <Search />
+    </div>
+  );
 }
 
 export default Index;
